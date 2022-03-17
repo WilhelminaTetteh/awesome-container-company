0447230f2a43b2ce82a6af3c7e0129490b0e4ec2
Renita Mwangachuchu <renita.mwangachuchu@gmail.com>
2022-03-17 10:30:11 -0400

    feat(solution): add button img; add solution card header

--- src/components/Solution.js
+++ src/components/Solution.js
@@ -5,6 +5,13 @@ const Solution = () => {
     <section className="solution">
       <h2 className="solution__header">Sustainability Delivered</h2>
       <p className="solution__text"><span className="solution__text-span">Awesome Container Company</span> replaces single-use packaging with reusable stainless steel containers</p>
+      <div className="solution__carousel">
+        <button className="solution__button" />
+        <div className="solution__card">
+
+        </div>
+        <button className="solution__button solution__button_isRight" />
+      </div>
     </section>
   );
 }
