3e0c25b5c667507928d06c8c1729e429c4239ce2
WilhelminaTetteh <73584400+WilhelminaTetteh@users.noreply.github.com>
2022-04-04 22:37:17 +0000

    minor: Validation

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -25,3 +25,53 @@ const FormInput = () => {
 };
 
 export default FormInput;
+
+// import React from 'react';
+
+// const FormInput = (props) => {
+// 	const { label, errorMessage, onChange, id, ...inputProps } = props;
+
+// 	return (
+// 		<div>
+// 			<div className='form__input-wrapper'>
+// 				<label className='form__input-label'>{label}</label>
+// 				<span className='form__input-error'>{errorMessage}</span>
+// 				<input
+// 					{...inputProps}
+// 					onChange={onChange}
+// 					type='text'
+// 					className='form__input'
+// 				/>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default FormInput;
+// import React from 'react';
+
+// const FormInput = () => {
+// 	return (
+// 		<div>
+// 			<div className='form__input-wrapper'>
+// 				<label className='form__input-label'>
+// 					Personal or company name *
+// 				</label>
+// 				<span className='form__input-error'>
+// 					Personal or company name * (This field is required)
+// 				</span>
+// 				<input type='text' className='form__input' />
+// 			</div>
+// 			{/* delete later */}
+// 			<div className='form__input-wrapper'>
+// 				<label className='form__input-label'>Email *</label>
+// 				<span className='form__input-error'>
+// 					Email * (This field is required)
+// 				</span>
+// 				<input type='text' className='form__input' />
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default FormInput;
