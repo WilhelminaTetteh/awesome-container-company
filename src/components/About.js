5cf0e7d3e0bedcd27d2e47acb69b23ef9d92c348
Renita M <85378780+renitaw@users.noreply.github.com>
2022-04-05 06:21:07 -0400

    feat(about): add mobile responsive design

--- src/components/About.js
+++ src/components/About.js
@@ -3,43 +3,23 @@ import image1 from '../images/about1.png';
 import image2 from '../images/about2.png';
 import image3 from '../images/about3.png';
 import image4 from '../images/about4.png';
-import arrow1 from '../images/arrow1.png';
-import arrow2 from '../images/arrow2.png';
-import arrow3 from '../images/arrow3.png';
-import arrow4 from '../images/arrow4.png';
-
-// aboutList = [
-// 	{
-// 		key: 1,
-// 		image: image1,
-// 		alt: '',
-// 		content: 
-// 	}
-// ]
 
 const About = ({ onContactButtonClick }) => {
 	return (
 		<section className='about' id='about'>
 			<h2 className='about__header'>How it works?</h2>
 			<div className='about__visual'>
-				<img
-					src={arrow4}
-					alt='diagonal down right arrow'
-					className='about__arrow about__arrow_4'
-				/>
 				<p className='about__text about__text_1'>
 					<img
 						src={image1}
-						alt=''
+						alt='cell phone hovering over pile of containers'
 						className='about__image about__image_1'
 					/>
 					01.
 					<br />
 					Restaurants request reusable containers via app
 				</p>
-				<img
-					src={arrow1}
-					alt='diagonal down right arrow'
+				<div
 					className='about__arrow about__arrow_1'
 				/>
 				<p className='about__text about__text_2'>
@@ -50,13 +30,9 @@ const About = ({ onContactButtonClick }) => {
 					/>	
 					02.
 					<br />
-					Deliver containers
-					<br />
-					to restaurants
+					Deliver containers to restaurants
 				</p>
-				<img
-					src={arrow2}
-					alt='diagonal down right arrow'
+				<div
 					className='about__arrow about__arrow_2'
 				/>
 				<p className='about__text about__text_3'>
@@ -67,13 +43,9 @@ const About = ({ onContactButtonClick }) => {
 					/>
 					03.
 					<br />
-					Pick up used containers from users
-					<br />
-					via app
+					Pick up used containers from users via app
 				</p>
-				<img
-					src={arrow3}
-					alt='diagonal down right arrow'
+				<div
 					className='about__arrow about__arrow_3'
 				/>
 				<p className='about__text about__text_4'>
@@ -84,10 +56,11 @@ const About = ({ onContactButtonClick }) => {
 					/>
 					04.
 					<br />
-					Wash containers
-					<br />
-					at commercial facility
+					Wash containers at commercial facility
 				</p>
+				<div
+					className='about__arrow about__arrow_4'
+				/>
 				<p className='about__center-text'>Circular ecosystem</p>
 			</div>
 			<button
