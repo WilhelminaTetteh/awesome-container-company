a99f97f13c4bd166cb60d676f8d69466a38f9582
Renita M <85378780+renitaw@users.noreply.github.com>
2022-04-06 19:22:46 -0400

    feat(chat): add rough draft of chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,37 +1,49 @@
 import React from "react";
+import user from "../images/user.svg";
+import acc from "../images/acc.svg";
 
 const chatHistory = [
   {
     content: "What do you do?",
-    author: "customer",
-    highlight: false
+    bubble: "",
+    text: ""
   },
   {
     content: "We provide reusable containers for restaurants, pick them up from users, clean them and give them back to your restaurant",
-    author: "acc",
-    highlight: false
+    bubble: "chat__bubble_acc",
+    text: "chat__message_highlight"
   },
   {
     content: "Awesome!",
-    author: "customer",
-    highlight: false
+    bubble: "",
+    text: ""
   },
   {
     content: "I want to work with you!",
-    author: "customer",
-    highlight: true
+    bubble: "chat__bubble_highlight",
+    text: "chat__message_highlight"
   },
   {
     content: "Cool! We operate in Singapore and are planning to open in Many onther countires soon!",
-    author: "acc",
-    highlight: false
+    bubble: "chat__bubble_acc",
+    text: "chat__message_highlight"
   },
 ]
 
 const Chat = () => {
   return (
     <section className="chat" id="chat">
-      <img className="chat__avatar_customer" alt="black and white smiley" />
+      <img className="chat__avatar chat__avatar_customer" alt="black and white smiley" src={user} />
+      <div className="chat__container">
+        {chatHistory.map(({content, bubble, text}) => {
+          return (
+            <div className={`chat__bubble ${bubble}`}>
+              <p className={`chat__message ${text}`}>{content}</p>
+            </div>
+          )
+        })}
+      </div>
+      <img className="chat__avatar chat__avatar_acc" alt="container company smiley" src={acc} />
     </section>
   )
 }
