8fcc6d3118fc167c48b834e754a866d6038c22c5
Renita M <85378780+renitaw@users.noreply.github.com>
2022-04-06 01:59:27 -0400

    feat(navbar): add mobile menu and stylings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,51 @@ import React from 'react';
 // import './Navbar.css';
 import logo from '../images/logo.svg';
 
+const navOptions = [
+	{
+		key: 1,
+		content: 'The Problem',
+		href: '#problem'
+	},
+	{
+		key: 2,
+		content: 'Sustainable Solution',
+		href: '#solution'
+	},
+	{
+		key: 3,
+		content: 'Impact',
+		href: '#impact'
+	},
+	{
+		key: 4,
+		content: 'Pricing',
+		href: '#pricing'
+	},
+	{
+		key: 5,
+		content: 'Team',
+		href: '#team'
+	},
+	{
+		key: 6,
+		content: 'Contacts',
+		href: '#footer'
+	}
+]
+
 const Navbar = ({ onContactButtonClick }) => {
+	const [isMenuOpen, setIsMenuOpen] = React.useState(true);
+
+	const onClick = () => {
+		setIsMenuOpen(!isMenuOpen);
+	}
+
+	const onMobileClick = () => {
+		onClick();
+		onContactButtonClick();
+	}
+
 	return (
 		<nav className='navbar'>
 			<div className='navbar__content'>
@@ -16,45 +60,68 @@ const Navbar = ({ onContactButtonClick }) => {
 						<p className='logo__text'>Awesome Container Company</p>
 					</a>
 				</div>
-				<ul className='navbar__items'>
-					<li className='navbar__item'>
-						<a className='navbar__link' href='#problem'>
-							The Problem
-						</a>
-					</li>
-					<li className='navbar__item'>
-						<a className='navbar__link' href='#solution'>
-							Sustainable Solution
-						</a>
-					</li>
-					<li className='navbar__item'>
-						<a className='navbar__link' href='#impact'>
-							Impact
-						</a>
-					</li>
-					<li className='navbar__item'>
-						<a className='navbar__link' href='#pricing'>
-							Pricing
-						</a>
-					</li>
-					<li className='navbar__item'>
-						<a className='navbar__link' href='#team'>
-							Team
-						</a>
-					</li>
-					<li className='navbar__item'>
-						<a className='navbar__link' href='#footer'>
-							Contacts
-						</a>
-					</li>
-					<li className='navbar__item'>
-						<button
-							onClick={onContactButtonClick}
-							className='navbar__button'>
-							Work with us
-						</button>
-					</li>
-				</ul>
+				<div className='navbar__right'>
+					<button 
+						aria-label='hamburger menu'
+						className={`navbar__menu ${isMenuOpen ? 'navbar__menu_active' : ''}`}
+						onClick={onClick}
+					/>
+					<ul className={`navbar__items ${isMenuOpen ? 'navbar__items_mobile' : ''}`}>
+						{navOptions.map(({key, content, href}) => {
+							return (
+								<li key={key} className={`navbar__item ${isMenuOpen ? 'navbar__item_mobile' : ''}`} onClick={onClick}>
+									<a className={`navbar__link ${isMenuOpen ? 'navbar__link_mobile' : ''}`} href={href}>
+										{content}
+									</a>
+								</li>
+							)
+						})}
+						{/* <li className='navbar__item'>
+							<a className={`navbar__link ${isMenuOpen ? 'navbar__link_mobile' : ''}`} href='#problem'>
+								The Problem
+							</a>
+						</li>
+						<li className='navbar__item'>
+							<a className={`navbar__link ${isMenuOpen ? 'navbar__link_mobile' : ''}`} href='#solution'>
+								Sustainable Solution
+							</a>
+						</li>
+						<li className='navbar__item'>
+							<a className={`navbar__link ${isMenuOpen ? 'navbar__link_mobile' : ''}`} href='#impact'>
+								Impact
+							</a>
+						</li>
+						<li className='navbar__item'>
+							<a className={`navbar__link ${isMenuOpen ? 'navbar__link_mobile' : ''}`} href='#pricing'>
+								Pricing
+							</a>
+						</li>
+						<li className='navbar__item'>
+							<a className={`navbar__link ${isMenuOpen ? 'navbar__link_mobile' : ''}`} href='#team'>
+								Team
+							</a>
+						</li>
+						<li className='navbar__item'>
+							<a className={`navbar__link ${isMenuOpen ? 'navbar__link_mobile' : ''}`} href='#footer'>
+								Contacts
+							</a>
+						</li> */}
+						<li className='navbar__item'>
+							<button
+								onClick={onMobileClick}
+								className={`navbar__button ${isMenuOpen ? 'navbar__button_mobile' : 'navbar__button_hide'}`}
+							>
+								Work with us
+							</button>	
+						</li>
+					</ul>
+					<button
+						onClick={onContactButtonClick}
+						className={`navbar__button ${isMenuOpen ? 'navbar__button_hide' : ''}`}
+					>
+						Work with us
+					</button>
+				</div>
 			</div>
 		</nav>
 	);
